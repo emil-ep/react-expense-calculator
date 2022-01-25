22a98069c72135293f3b531ce5601a257048457b
Emil <emilpradeep@gmail.com>
2022-01-25 18:30:14 +0530

    made the expenseItem jsx element dynamic

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -8,9 +8,7 @@ const Expenses = (props) => {
   const [yearFilter, setYearFilter] = useState("2020");
 
   const yearFilterChangeHandler = (selectedYear) => {
-    // setSelectedYear(year);
     setYearFilter(selectedYear);
-    // console.log(selectedYear);
   };
 
   return (
@@ -20,26 +18,13 @@ const Expenses = (props) => {
           selected={yearFilter}
           onYearSelected={yearFilterChangeHandler}
         ></ExpenseFilter>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        ></ExpenseItem>
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        ))}
       </Card>
     </div>
   );
